d14d19f1144a5c5650080e94cadf5271e56f4b62
Warren Bailey <warren@warrenbailey.net>
2017-04-26 09:35:12 +0100

    Adding logging for DB connection details

--- server.py
+++ server.py
@@ -73,5 +73,6 @@ if __name__ == '__main__':
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
     handler = logging.StreamHandler(sys.stdout)
     app.logger.addHandler(handler)
+    app.logger.info("DB connection details host: %(host)s port: %(port)s dbname: %(dbname)s user: %(user)s password: %(password)s", get_dsn(settings))
     port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
