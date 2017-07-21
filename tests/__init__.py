b30f4b4c1ba3e4735ff9d8367b69ec62ebbe2957
Warren Bailey <warren@warrenbailey.net>
2017-07-21 08:30:24 +0100

    Removing need for a database on Travis (#35)
    * Removing need for a database
    
    * Don't initialise the cache database in tests
    
    * Updating changelog

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ import settings
 from sequences import create_sequences
 
 # Launch new PostgreSQL server
-Postgres = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
+Postgres = testing.postgresql.PostgresqlFactory(cache_initialized_db=False)
 postgresql = Postgres()
 settings.DB_URL = postgresql.url()
 engine = create_engine(postgresql.url())
